e6a6307d4b63befd02beb30aab261131725eb225
DBeldiativ <166904587+DBeldiativ@users.noreply.github.com>
2024-11-19 00:58:12 +0100

    Update BE

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,19 @@
 const checkRole = (requiredRoles) => (req, res, next) => {
-  const userRole = req.headers["role"]; // Simulace role z hlavičky požadavku
+  const userRole = req.headers["role"]; 
 
-  // Pokud je očekávána pouze jedna role, převedeme ji na pole pro snadnější kontrolu
+  
   if (!Array.isArray(requiredRoles)) {
     requiredRoles = [requiredRoles];
   }
 
-  // Ověřujeme, zda role uživatele je v seznamu požadovaných rolí
+  
   if (!requiredRoles.includes(userRole)) {
     return res.status(403).json({
       uuAppErrorMap: { authorizationError: "Přístup odepřen" },
     });
   }
 
-  next(); // Pokud je role platná, pokračujeme na další middleware nebo endpoint
+  next(); 
 };
 
 module.exports = checkRole;
